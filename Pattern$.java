b03947e80b3de8fc49940910ce42ff20072a203c
MTCA28714 <126247128+MTCA28714@users.noreply.github.com>
2023-02-27 17:34:56 +0530

    Add files via upload

---
+++ Pattern$.java
@@ -0,0 +1,22 @@
+class Pattern$
+{
+	static void printpatternoneRow(char ch,int nc)
+{
+		for(int i=0;i<nc;i++)
+	{
+			System.out.print(ch);
+	}
+	System.out.print("\n");
+}
+static void printpattern(int nr,char ch)
+	{
+	for(int i=1;i<=nr;i++)
+		{
+		printpatternoneRow(ch,i);
+		}
+}
+public static void main(String[]args)
+	{
+	printpattern(6,'$');
+	}
+}
