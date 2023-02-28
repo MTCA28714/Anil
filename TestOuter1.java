649b6de3ceb041214361a7958ae85e89f37d0429
MTCA28714 <126247128+MTCA28714@users.noreply.github.com>
2023-02-28 16:23:23 +0530

    Add files via upload

---
+++ TestOuter1.java
@@ -0,0 +1,19 @@
+class TestOuter1
+{
+	static int data=30;
+	static class Inner
+	{
+		void msg();
+		{
+			static void msg()
+			{
+			System.out.println("data is"+data);
+	}
+}
+public static void main(String[]args)
+	{
+	TestOuter.Inner obj=new TestOuter.Inner();
+	obj.msg();
+	}
+}
+}
