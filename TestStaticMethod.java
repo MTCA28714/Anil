1316a526f3f2d97eaa2974ded3d8b27bc5cd358a
MTCA28714 <126247128+MTCA28714@users.noreply.github.com>
2023-02-28 13:17:50 +0530

    Add files via upload

---
+++ TestStaticMethod.java
@@ -0,0 +1,32 @@
+class Student
+{
+	int rollno;
+	String name;
+	static String college="ITS";
+	static void change()
+	{
+		college="BBDIT";
+}
+Student(int r,String n)
+	{
+	rollno=r;
+	name=n;
+	}
+	void display()
+	{
+		System.out.println(rollno+" "+name+" "+college);
+	}
+}
+class TestStaticMethod
+{
+	public static void main(String[]args)
+	{
+		Student.change();
+		Student s1=new Student(111,"Anil");
+		Student s2=new Student(222,"Ganesh");
+		Student s3=new Student(333,"Arun");
+		s1.display();
+		s2.display();
+		s3.display();
+}
+}
