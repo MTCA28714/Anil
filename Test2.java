698d2134fe15c26d34979aca20955ca9c1a914de
MTCA28714 <126247128+MTCA28714@users.noreply.github.com>
2023-03-01 16:18:50 +0530

    Add files via upload

---
+++ Test2.java
@@ -0,0 +1,26 @@
+class MyDate
+{
+	public int day;
+	private int month;
+	public int year;
+	public int get_month()
+	{
+		return month;
+	}
+	public void set_month(int m)
+	{
+		month=m;
+	}
+
+}
+class Test2
+{
+	public static void main(String args[])
+	{
+		MyDate ob1=new MyDate();
+		ob1.day=10;
+		ob1.set_month(2);
+		ob1.year=2023;
+		System.out.println(ob1.day+"_"+ob1.get_month()+"_"+ob1.year);
+	}
+}
