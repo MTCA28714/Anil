a06bed34120ce99cffe9469a837232950d8d063a
MTCA28714 <126247128+MTCA28714@users.noreply.github.com>
2023-03-04 17:47:05 +0530

    Add files via upload

---
+++ Address.java
@@ -0,0 +1,10 @@
+public class Address
+{
+	String City,State,Country;
+	public Address(String city,String state,String country)
+	{
+		this.City=city;
+		this.State=state;
+		this.Country=country;
+}
+}
