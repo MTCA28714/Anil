41a6f3aa705f2659613c5f90dca55e002c14101e
MTCA28714 <126247128+MTCA28714@users.noreply.github.com>
2023-03-07 15:18:26 +0530

    Add files via upload

---
+++ Cubeofarray.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Cubeofarray
+{
+ public static void main(String[] args) 
+	{
+	 Scanner sc=new Scanner(System.in);
+	 int cube[]=new int[10];
+		System.out.println("Enter 10 array elements");
+		int Arr[]=new int[10];
+		for(int i=0;i<Arr.length;i++)
+			Arr[i]=sc.nextInt();
+		System.out.println("cube of Numbers:");
+		for(int j=0;j<Arr.length;j++)
+		{
+			cube[j]=Arr[j]*Arr[j]*Arr[j];
+				System.out.println(cube[j]);
+		}
+	}
+}
+			
